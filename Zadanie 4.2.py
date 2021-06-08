e3e3daade06bca32df3a966a4f46a9a6aa2b378e
Piotr Polak <polak.piotr94@gmail.com>
2021-06-08 03:17:51 +0200

    Corrected Zadanie 4.2.py

--- Zadanie 4.2.py
+++ Zadanie 4.2.py
@@ -1,10 +1,9 @@
 def palindrome_checker(word: str):
-    return word == word[::-1]
-
     """
         Check palindrome based on typed word
-
     """
+    return word == word[::-1]
+
 
 type_your_word = "kajak"
 result = palindrome_checker(type_your_word)
